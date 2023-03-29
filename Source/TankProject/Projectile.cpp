9217ce2d6b087114cdb825d0774c722e31bc73cc
Stepan <Steven.nnov@gmail.com>
2023-03-29 15:00:39 +0300

    problem with projectile class

---
+++ Source/TankProject/Projectile.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+#include "Components/StaticMeshComponent.h"
+#include "TimerManager.h"
+#include "Components/SceneComponent.h"
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+	USceneComponent* sceeneCpm = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = sceeneCpm;
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ProjectileMesh"));
+	ProjectileMesh->SetupAttachment(RootComponent);
+	ProjectileMesh->OnComponentBeginOverlap.AddDynamic(this, &AProjectile::OnMeshOverlapBegin);
+}
+
+void AProjectile::Start()
+{
+	GetWorld()->GetTimerManager().SetTimer(MovementTimer, this, &AProjectile::Move, MoveRate, true, 0);
+}
+
+void AProjectile::Move()
+{
+	FVector nextPosition = GetActorLocation() + GetActorForwardVector() * MoveSpeed * MoveRate;
+	SetActorLocation(nextPosition);
+}
+
+void AProjectile::OnMeshOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const	FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Projectile %s collided with %s. "), *GetName(), *OtherActor->GetName());
+}
---
+++ Source/TankProject/Projectile.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class TANKPROJECT_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+	
+	void Start();
+
+protected:
+	// Called when the game starts or when spawned
+	//virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	class UStaticMeshComponent* ProjectileMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement|Speed")
+	float MoveSpeed = 100;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Movement|Speed")
+	float MoveRate = 0.005f; // position update frequency
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Damage")
+	float Damage = 1;
+
+	FTimerHandle MovementTimer;
+
+	UFUNCTION()
+	void Move();
+
+	UFUNCTION()
+	void OnMeshOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
